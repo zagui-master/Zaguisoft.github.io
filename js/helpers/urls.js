34ad26a9b67f7fe709d359333f4b545770cdeb33
zagui-master <github.correo@gmail.com>
2023-04-18 10:18:43 -0500

    add a new urls in urls.js file

--- js/helpers/urls.js
+++ js/helpers/urls.js
@@ -1,1 +1,1 @@
-export const url={log_in:"http://127.0.0.1:5502/src/index.html",sign_up:"http://127.0.0.1:5502/src/pages/sign-up.html",courses:"http://127.0.0.1:5502/src/pages/courses.html",payment:"http://127.0.0.1:5502/src/pages/payment.html",admin:"http://127.0.0.1:5502/src/pages/admy.html"};
+export const url={log_in:"index.html",sign_up:"../pages/sign-up.html",courses:"../pages/courses.html",payment:"../pages/payment.html",admin:"../pages/admy.html"};
